3414471e23e4fc81185605c6db6f3e551cb1ab95
jodin <jodinsmart@gmail.com>
2022-03-15 10:25:38 -0700

    My state

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include <unistd.h> 
-#include "main.h"
+#include <main.h>
 /**
  * main - Putchar
  * 
  * _putchar print the characters given
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-        _putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
