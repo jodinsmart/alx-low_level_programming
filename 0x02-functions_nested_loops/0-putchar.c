df17663eb078b8c5b053fb06bbc00e7ab25ffcca
jodin <jodinsmart@gmail.com>
2022-03-15 09:47:40 -0700

    My state

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
-#include <main.h>
-#include <_putchar.c>
+#include "main.h"
+#include "_putchar.c"
 /**
- * main - entry point.
+ * main - prints _putchar, followed by a new line
  * Description: program starts and ends here.
  * Return: 0 on success.
  */
+
 int main(void)
 {
-	char text[8] = "_putchar";
-	int i;
+        char text[8] = "_putchar";
+        int i;
+
 	for (i = 0; i < 8: i++)
 	{
-	_putchar(text[i]);
+	   _putchar(text[i]);
 	}
 	_putchar('\n');
+	
 	return (0);
 }
