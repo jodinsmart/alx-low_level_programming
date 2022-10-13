4e173536e4eb622e8fe7327246080f139050b6cd
jodinmart <jodinsmart@gmail.com>
2022-10-14 00:01:33 +0100

    C

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("\n");
+	return (0);
+}
