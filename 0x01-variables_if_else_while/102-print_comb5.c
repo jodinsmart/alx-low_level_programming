746e5214da0f211bb27108aa5815f876cb13b34b
jodinmart <jodinsmart@gmail.com>
2022-10-16 23:47:44 +0100

    all

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
-
 /**
- * main - Print a string
- * Description: use printf
- * Return: 0
- */
+* main - This program prints all two digit combinations with ,
+* space and a new line
+* Return: returns 0
+*/
 int main(void)
 {
-	printf("\n");
+	int digit_1, digit_2;
+
+	for (digit_1 = 0; digit_1 <= 98; digit_1++)
+	{
+		for (digit_2 = digit_1 + 1; digit_2 <= 99; digit_2++)
+		{
+			putchar((digit_1 / 10) + '0');
+			putchar((digit_1 % 10) + '0');
+			putchar(32);
+			putchar((digit_2 / 10) + '0');
+			putchar((digit_2 % 10) + '0');
+
+			if (digit_1 / 10 != 9 || digit_1 % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
 	return (0);
 }
