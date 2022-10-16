904ebc70289c9ea5f181fe54266407c504e8c247
jodinmart <jodinsmart@gmail.com>
2022-10-16 23:45:40 +0100

    all

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - Print a string
- * Description: use printf
- * Return: 0
- */
+* main - This program prints all three digit combinations with ,
+* space and new line
+* Return: 0
+*/
 int main(void)
 {
-	printf("\n");
+	int digit_1, digit_2, digit_3;
+
+	for (digit_1 = 0; digit_1 < 9; digit_1++)
+	{
+		for (digit_2 = digit_1 + 1; digit_2 < 9; digit_2++)
+		{
+			for (digit_3 = digit_2 + 1; digit_3 < 10; digit_3++)
+			{
+
+				putchar((digit_1 % 10) + '0');
+				putchar((digit_2 % 10) + '0');
+				putchar((digit_3 % 10) + '0');
+
+				if (digit_1 == 7 && digit_2 == 8 && digit_3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
