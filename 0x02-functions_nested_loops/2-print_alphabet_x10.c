71944986eb45d8fb7bac7384c70c7fcf3b20fdf1
jodinmart <jodinsmart@gmail.com>
2022-10-18 12:19:06 +0100

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - main block
+ *  Description: You can only use the putchar function
+ *  Return: 0
+ */
+void print_alphabet_x10(void);
+int main(void)
+{
+
+	
+
+
+
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - main block
+ *  Description: You can only use the putchar function
+ *  Return: 0
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+
+
+}
