4bac665d396a8f2cb9aa5a4dd393f108b94a77ac
jodinmart <jodinsmart@gmail.com>
2022-10-18 10:33:09 +0100

    you

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Confirmed)
+ */
+int main(void)
+{	
+	
+	char sc = "Smoov";
+	
+	printf("_putchar\n");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - main block
+ *  Description: You can only use the putchar function
+ *  Return: 0
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+return (0);
+}
