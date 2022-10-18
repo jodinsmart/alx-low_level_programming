89023a4b8eaa0738797b25d3bfdd59a1dff3c359
jodinmart <jodinsmart@gmail.com>
2022-10-18 20:22:04 +0100

    12

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- *  main - main block
- *  Description: You can only use the putchar function
- *  Return: 0
- */
-void print_alphabet(void);
+ * _isalpha - checks for alphabetic character
+ * @c: character that is going to be checked
+ * Return: 1 if @c is a letter, lowercase or uppercase, otherwise 0.
+**/
 
-int main(void)
+int _isalpha(int c)
 {
-
-
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
