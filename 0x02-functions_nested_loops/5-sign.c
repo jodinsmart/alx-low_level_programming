c4bac1173c9bc69b717f314cbc2cb21ecc6d532f
jodinmart <jodinsmart@gmail.com>
2022-10-19 19:02:24 +0100

    cool

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- *  main - main block
- *  Description: You can only use the putchar function
- *  Return: 0
- */
-void print_alphabet(void);
+#include "main.h"
 
-int main(void)
+/**
+ * print_sign - prints the sign of a number
+ * @n: number that is going to be checked
+ * Return: 1 if @n > zero, 0 if @n = zero and -1 if @n < zero
+**/
+
+int print_sign(int n)
 {
+	int ret;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		ret = 1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		ret = 0;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		ret = -1;
+	}
+	return (ret);
+}
