400751f997e8b427f679fd329b262b7b6db72e1f
jodinmart <jodinsmart@gmail.com>
2022-10-24 21:40:21 +0100

    chill

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * print_rev - prints a string, in reverse.
+ * @s: parameter
+ * return: Void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+
+}
