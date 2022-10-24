51e390cb2c06701e8228f204580411182a50228d
jodinmart <jodinsmart@gmail.com>
2022-10-24 17:57:59 +0100

    chill

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: the parameter
+ * Return: an integer
+**/
+
+int _strlen(char *s)
+
+{
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+
+}
